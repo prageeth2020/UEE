b62bf7bd330ff7a29e771191abb0e1d6ceec8731
Piyumi14 <lbpmdayarathna@gmail.com>
2020-10-04 11:58:02 +0530

    Added Modified Interfaces for dashbords

--- app/src/main/java/com/example/uee/Expenses.java
+++ app/src/main/java/com/example/uee/Expenses.java
@@ -3,12 +3,74 @@ package com.example.uee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Expenses extends AppCompatActivity {
+    private Spinner spinner4, spinner2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
+        addItemsOnSpinner2();
+        addItemsOnSpinner4();
     }
+
+    public void addItemsOnSpinner2() {
+
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        List<String> list = new ArrayList<String>();
+        list.add("");
+        list.add("2015");
+        list.add("2016");
+        list.add("2017");
+        list.add("2018");
+        list.add("2019");
+        list.add("2020");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(dataAdapter);
+    }
+
+    public void addItemsOnSpinner4() {
+
+        spinner4 = (Spinner) findViewById(R.id.spinner4);
+        List<String> list = new ArrayList<String>();
+        list.add("");
+        list.add("January");
+        list.add("February");
+        list.add("March");
+        list.add("April");
+        list.add("May");
+        list.add("June");
+        list.add("July");
+        list.add("August");
+        list.add("September");
+        list.add("October");
+        list.add("November");
+        list.add("December");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(dataAdapter);
+    }
+
+
 }
