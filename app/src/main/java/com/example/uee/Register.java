3db8a738d56439ad169bfc4cd799ef36c2a883ee
SunethChinthaka <sunethedu@gmail.com>
2020-10-05 19:57:22 +0530

    Register Added

---
+++ app/src/main/java/com/example/uee/Register.java
@@ -0,0 +1,28 @@
+package com.example.uee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Register extends AppCompatActivity {
+
+    Button buttonRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        buttonRegister=findViewById(R.id.btnRegister);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
