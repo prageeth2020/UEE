d84e468651d14e2b8ba6caac73d61909cf5059b8
SunethChinthaka <sunethedu@gmail.com>
2020-10-05 19:31:29 +0530

    Start Screen

---
+++ app/src/main/java/com/example/uee/StartScreen.java
@@ -0,0 +1,37 @@
+package com.example.uee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartScreen extends AppCompatActivity {
+
+    Button buttonL,buttonR;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+
+//        buttonL=findViewById(R.id.btnLog);
+//        buttonL.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(getApplicationContext(),Login.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        buttonR=findViewById(R.id.btnReg);
+//        buttonR.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(getApplicationContext(),Register.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
