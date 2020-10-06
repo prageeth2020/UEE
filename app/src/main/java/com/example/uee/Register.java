915d3020266d48ab180333e99222e65b15328f24
SunethChinthaka <sunethedu@gmail.com>
2020-10-06 20:59:32 +0530

    Register Link Added

--- app/src/main/java/com/example/uee/Register.java
+++ app/src/main/java/com/example/uee/Register.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
     Button buttonRegister;
+    TextView linkLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,14 @@ public class Register extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(),Login.class);
                 startActivity(intent);
+            }
+        });
+
+        linkLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent=new Intent(getApplicationContext(),Login.class);
+                startActivity(loginIntent);
             }
         });
     }
