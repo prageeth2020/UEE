991c39f40da1e5a13e401380eee25c0682d74ddf
SunethChinthaka <sunethedu@gmail.com>
2020-10-06 20:58:01 +0530

    Login Link Added

--- app/src/main/java/com/example/uee/Login.java
+++ app/src/main/java/com/example/uee/Login.java
@@ -6,25 +6,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
     Button buttonLogin;
+    TextView linkRegister,linkForgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        buttonLogin=findViewById(R.id.btnLogin);
-//        buttonLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent=new Intent(getApplicationContext(),Home.class);
-//                startActivity(intent);
-//            }
-//        });
+        buttonLogin=findViewById(R.id.btnLogin);
+        linkRegister=findViewById(R.id.linkReg);
+        linkForgot=findViewById(R.id.linkForgotPw);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Home.class);
+                startActivity(intent);
+            }
+        });
+
+        linkRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent=new Intent(getApplicationContext(),Register.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        linkForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent forgotIntent=new Intent(getApplicationContext(),ForgotPassword.class);
+                startActivity(forgotIntent);
+            }
+        });
     }
 
     public void displaySuccess(View view) {
