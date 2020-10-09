d689b069f171565c4c064722d69736d43fc6c892
Piyumi14 <lbpmdayarathna@gmail.com>
2020-10-09 08:17:55 +0530

    Mark Attendance completed

--- app/src/main/java/com/example/uee/MarkAttendence.java
+++ app/src/main/java/com/example/uee/MarkAttendence.java
@@ -3,7 +3,10 @@ package com.example.uee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -11,6 +14,8 @@ import java.util.List;
 
 public class MarkAttendence extends AppCompatActivity {
     private Spinner spinner1, spinner2;
+    LinearLayout linearLayout3;
+    Button button16;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,19 @@ public class MarkAttendence extends AppCompatActivity {
         setContentView(R.layout.activity_mark_attendence);
         addItemsOnSpinner1();
         addItemsOnSpinner2();
+
+        button16 = findViewById(R.id.button16);
+        linearLayout3 = findViewById(R.id.tableLayout);
+
+        button16.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(linearLayout3.getVisibility()==View.VISIBLE){
+                    linearLayout3.setVisibility(View.GONE);
+                }else{
+                    linearLayout3.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     public void addItemsOnSpinner1() {
@@ -25,11 +43,11 @@ public class MarkAttendence extends AppCompatActivity {
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         List<String> list = new ArrayList<String>();
         list.add("");
-        list.add("Batch 1");
-        list.add("Batch 2");
-        list.add("Batch 3");
-        list.add("Batch 4");
-        list.add("Batch 5");
+        list.add("   Batch 1");
+        list.add("   Batch 2");
+        list.add("   Batch 3");
+        list.add("   Batch 4");
+        list.add("   Batch 5");
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -41,11 +59,11 @@ public class MarkAttendence extends AppCompatActivity {
         spinner2 = (Spinner) findViewById(R.id.spinner2);
         List<String> list = new ArrayList<String>();
         list.add("");
-        list.add("S001");
-        list.add("S002");
-        list.add("S003");
-        list.add("S004");
-        list.add("S005");
+        list.add("   S001");
+        list.add("   S002");
+        list.add("   S003");
+        list.add("   S004");
+        list.add("   S005");
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
