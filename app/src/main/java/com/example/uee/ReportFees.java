a1c329b64a7ee89650187ba06b33267451036841
Piyumi14 <lbpmdayarathna@gmail.com>
2020-10-09 07:05:11 +0530

    Fee Report completed

--- app/src/main/java/com/example/uee/ReportFees.java
+++ app/src/main/java/com/example/uee/ReportFees.java
@@ -3,7 +3,10 @@ package com.example.uee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -11,12 +14,26 @@ import java.util.List;
 
 public class ReportFees extends AppCompatActivity {
     private Spinner spinner1;
+    LinearLayout linearLayout3;
+    Button button16;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_fees);
         addItemsOnSpinner1();
+        button16 = findViewById(R.id.button16);
+        linearLayout3 = findViewById(R.id.tableLayout);
+
+        button16.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(linearLayout3.getVisibility()==View.VISIBLE){
+                    linearLayout3.setVisibility(View.GONE);
+                }else{
+                    linearLayout3.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     public void addItemsOnSpinner1() {
