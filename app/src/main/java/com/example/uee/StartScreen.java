b00949f089bc49e17215461ea2ff9240be135c22
SunethChinthaka <sunethedu@gmail.com>
2020-10-10 11:04:46 +0530

    Uncommented StartScreen

--- app/src/main/java/com/example/uee/StartScreen.java
+++ app/src/main/java/com/example/uee/StartScreen.java
@@ -16,22 +16,22 @@ public class StartScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
 
-//        buttonL=findViewById(R.id.btnLog);
-//        buttonL.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent=new Intent(getApplicationContext(),Login.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        buttonR=findViewById(R.id.btnReg);
-//        buttonR.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent=new Intent(getApplicationContext(),Register.class);
-//                startActivity(intent);
-//            }
-//        });
+        buttonL=findViewById(R.id.btnLog);
+        buttonL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonR=findViewById(R.id.btnReg);
+        buttonR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
